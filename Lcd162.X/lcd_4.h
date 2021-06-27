512c7808a6a081a67ff5ead69eb82ce7603f0cde
Shiv2793 <49137981+Shiv2793@users.noreply.github.com>
2021-06-27 17:42:41 +0530

    Update lcd_4.h

--- Lcd162.X/lcd_4.h
+++ Lcd162.X/lcd_4.h
@@ -2,54 +2,56 @@
 
 //assume that you have defined connections as
 //DDRB=0xFF;
-//PB0 is en
-//PB1 is rs
+//PC0 is en
+//PC1 is rs
 //PB4 to PB7 are 4 bit data
-void setup1()
+
+    void setup1()
 {
-    DDRB=0xFF;  
+    DDRB=0xFF;
+    DDRC|=(1<<PC1);//rs
+    DDRC|=(1<<PC0);//en  
 }
 void lcd_cmd1(unsigned char val)
 {   PORTB=(PORTB & 0x0F)|(val & 0xF0);
-    PORTB&=~(1<<PB1);
-    PORTB|=(1<<PB0);
+    PORTC&=~(1<<PC1);
+    PORTC|=(1<<PC0);
     _delay_ms(100);
-    PORTB&=~(1<<PB0);
+    PORTC&=~(1<<PC0);
     _delay_ms(200);
     
     PORTB=(PORTB & 0x0F )|(val<<4);
-     PORTB&=~(1<<PB1);
-    PORTB|=(1<<PB0);
+     PORTC&=~(1<<PC1);
+    PORTC|=(1<<PC0);
     _delay_ms(100);
-    PORTB&=~(1<<PB0);
+    PORTC&=~(1<<PC0);
     _delay_ms(200);
 }
 void lcd_data1(unsigned char data)
 {
      PORTB=(PORTB & 0x0F)|(data & 0xF0);
-    PORTB|=(1<<PB1);
-    PORTB|=(1<<PB0);
+    PORTC|=(1<<PC1);
+    PORTC|=(1<<PC0);
     _delay_ms(100);
-    PORTB&=~(1<<PB0);
+    PORTC&=~(1<<PC0);
     _delay_ms(200);
     
     PORTB=(PORTB & 0x0F )|(data<<4);
-     PORTB|=(1<<PB1);
-    PORTB|=(1<<PB0);
+     PORTC|=(1<<PC1);
+    PORTC|=(1<<PC0);
     _delay_ms(100);
-    PORTB&=~(1<<PB0);
+    PORTC&=~(1<<PC0);
     _delay_ms(200);
 }
 void lcd_init1()
 {
-    lcd_cmd1(0x38);
+   lcd_cmd1(0x02);
+    _delay_ms(300);
+    lcd_cmd1(0x28);
    _delay_ms(300);
-    lcd_cmd1(0x0E);
-    _delay_ms(300);
-    lcd_cmd1(0x0C);
-    _delay_ms(300);
     lcd_cmd1(0x01);
     _delay_ms(300);
-    lcd_cmd1(0xC0);
+    lcd_cmd1(0x0E);
     _delay_ms(300);
+    lcd_cmd1(0x80);
 }
